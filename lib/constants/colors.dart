fccfcce316a8a591feac3bb782a9d0e919ce3d76
Yohan Pasindu <yohanpersonalkr@gmail.com>
2025-01-04 09:29:20 +0530

    Set App Colors, Strings & Image and Ion paths

---
+++ lib/constants/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+const Color yPurpleColor = Color(0xFF7F3DFF);
+const Color yGreyColor = Color(0xFFEEEEEE);
+const Color yWhiteColor = Color(0xFFFFFFFF);
+const Color yBlackColor = Color(0xFF000000);
+const Color yGreenColor = Color(0xFF00A86B);
+const Color yOrangeColor = Color(0xFFFD3C4A);
+const Color yYellowColor = Color(0xFFFCAC12);
+const Color yCreamColor = Color(0xFFFCAC12);
